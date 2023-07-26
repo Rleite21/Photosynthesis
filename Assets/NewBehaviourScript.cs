bb8924cd3af1e77ea8cab1179178abf500c3e3a2
Tomás Oliveira <117669703+Tomaz612@users.noreply.github.com>
2023-07-26 19:42:47 +0100

    comm

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
+    [SerializeField] private string newgame;
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
-    public void Play()
+    public void NewGame()
     {
-
+        painelMenuInicial.SetActive(false);
+        SceneManager.LoadScene(newgame);
     }
 
     public void Settings()
